bfb2f6f96b23a7165d9d59e4023692081fc3556f
Christian Catchpole <christian@catchpole.net>
2015-10-05 21:25:14 +1100

    first draft of magical compact bean codec

--- src/test/java/net/catchpole/B9/codec/bean/GnarlyBean.java
+++ src/test/java/net/catchpole/B9/codec/bean/GnarlyBean.java
@@ -34,18 +34,18 @@ public class GnarlyBean {
         if (s1 != that.s1) return false;
         if (i1 != that.i1) return false;
         if (l1 != that.l1) return false;
-//        if (!Almost.equals(that.f1, f1, 1)) return false;
-//        if (!Almost.equals(that.d1, d1, 1)) return false;
+        if (Float.compare(that.f1, f1) != 0) return false;
+        if (Double.compare(that.d1, d1) != 0) return false;
+        if (maya != null ? !maya.equals(that.maya) : that.maya != null) return false;
+        if (lucinda != null ? !lucinda.equals(that.lucinda) : that.lucinda != null) return false;
         if (b2 != null ? !b2.equals(that.b2) : that.b2 != null) return false;
         if (by2 != null ? !by2.equals(that.by2) : that.by2 != null) return false;
         if (c2 != null ? !c2.equals(that.c2) : that.c2 != null) return false;
         if (s2 != null ? !s2.equals(that.s2) : that.s2 != null) return false;
         if (i2 != null ? !i2.equals(that.i2) : that.i2 != null) return false;
         if (l2 != null ? !l2.equals(that.l2) : that.l2 != null) return false;
-//        if (f2 != null ? !Almost.equals(f2, this.f2, 1) : that.f2 != null) return false;
-//        if (d2 != null ? !Almost.equals(d2, this.d2, 1) : that.d2 != null) return false;
-        if (maya != null ? !maya.equals(that.maya) : that.maya != null) return false;
-        if (lucinda != null ? !lucinda.equals(that.lucinda) : that.lucinda != null) return false;
+        if (f2 != null ? !f2.equals(that.f2) : that.f2 != null) return false;
+        if (d2 != null ? !d2.equals(that.d2) : that.d2 != null) return false;
         return !(string != null ? !string.equals(that.string) : that.string != null);
 
     }
