ec27afc4a7c710b01c6954c4a975c1043721cb1d
Christian Catchpole <christian@catchpole.net>
2016-01-23 09:06:05 +1000

    disable integration test

--- B9-core/src/test/java/net/catchpole/B9/devices/rockblock/RockBlockTest.java
+++ B9-core/src/test/java/net/catchpole/B9/devices/rockblock/RockBlockTest.java
@@ -1,14 +1,17 @@
 package net.catchpole.B9.devices.rockblock;
 
 import net.catchpole.B9.devices.serial.SocketSerialPort;
+import org.junit.Ignore;
 import org.junit.Test;
 
+@Ignore
 public class RockBlockTest {
     @Test
     public void test() throws Exception {
         RockBlock rockBlock = new RockBlock(new SocketSerialPort("localhost", 4000));
         rockBlock.connect();
-
+        System.out.println(rockBlock.hasReception());
+        System.out.println(rockBlock.getStatus());
         Thread.sleep(5000);
     }
 }
